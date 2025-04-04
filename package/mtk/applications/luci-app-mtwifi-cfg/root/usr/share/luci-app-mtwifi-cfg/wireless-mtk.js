d41c390b8ffbfd8a2dd3797729ef23ccf37860a9
Husky <hyiy666@gmail.com>
2025-04-04 10:47:44 +0800

    mtwifi-cfg:add Ft and BandSteering support.

--- package/mtk/applications/luci-app-mtwifi-cfg/root/usr/share/luci-app-mtwifi-cfg/wireless-mtk.js
+++ package/mtk/applications/luci-app-mtwifi-cfg/root/usr/share/luci-app-mtwifi-cfg/wireless-mtk.js
@@ -1048,7 +1048,10 @@ return view.extend({
 					{
 						o = ss.taboption('advanced', form.Flag, 'whnat', _('Wireless HWNAT'));
 						o.default = o.enabled;
-	
+
+						o = ss.taboption('advanced', form.Flag, 'bandsteering', _('Band Steering'));
+						o.default = o.disabled;
+
 						o = ss.taboption('advanced', form.Value, 'beacon_int', _('Beacon Interval'));
 						o.optional = true;
 						o.datatype = 'range(20,999)';
@@ -1319,6 +1322,10 @@ return view.extend({
 					o.default = o.enabled;
 					o.depends('mode', 'ap');
 
+					o = ss.taboption('advanced', form.Flag, 'ieee80211r', _('802.11r'), _('only supports mt_wifi driver'));
+					o.default = o.disabled;
+					o.depends('mode', 'ap');
+
 					o = ss.taboption('advanced', form.Value, 'wpa_group_rekey', _('Time interval for rekeying GTK'), _('sec'));
 					o.optional    = true;
 					o.placeholder = 3600;
@@ -1329,6 +1336,18 @@ return view.extend({
 					o.optional    = true;
 					o.placeholder = 0;
 					o.datatype = 'range(-100,0)';
+					o.depends('mode', 'ap');
+
+					o = ss.taboption('advanced', form.Value, 'steeringthresold', _('802.11V roam steering threshold'), _('dBm'));
+					o.optional    = true;
+					o.placeholder = 0;
+					o.datatype = 'range(-100,0)';
+					o.depends('mode', 'ap');
+
+					o = ss.taboption('advanced', form.DynamicList, 'steeringbssid',_('802.11V roam target bssid'), _('MAC-List'));
+					o.datatype = 'macaddr';
+					o.optional    = true;
+					o.placeholder = 0;
 					o.depends('mode', 'ap');
 
 					o = ss.taboption('advanced', form.Value, 'assocthres', _('Station associate threshold'), _('dBm'));
