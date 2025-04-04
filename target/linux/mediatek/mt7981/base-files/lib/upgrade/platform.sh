e8148e948001f3e44498b1bb7be57802a31c03d1
Husky <hyiy666@gmail.com>
2025-04-04 11:27:48 +0800

    add jcg q30 pro support.

--- target/linux/mediatek/mt7981/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/mt7981/base-files/lib/upgrade/platform.sh
@@ -202,7 +202,8 @@ platform_do_upgrade() {
 	*cetron,ct3003* |\
 	*clt,r30b1* |\
 	*imou,lc-hx3001* |\
-	*jcg,q30* |\
+	jcg,q30 |\
+	jcg,q30-pro |\
 	nradio,wt9103 |\
 	cmcc,a10 |\
 	cmcc,rax3000m |\
@@ -258,7 +259,8 @@ platform_check_image() {
 	*cetron,ct3003* |\
 	*clt,r30b1* |\
 	*imou,lc-hx3001* |\
-	*jcg,q30* |\
+	jcg,q30 |\
+	jcg,q30-pro |\
 	cmcc,a10 |\
 	cmcc,rax3000m* |\
 	cmcc,xr30* |\
