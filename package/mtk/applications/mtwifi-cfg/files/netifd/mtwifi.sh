a492dd6afd3fea419cdf1c888d62929c6374be31
FlawShatter <hyiy666@gmail.com>
2025-05-16 21:33:45 +0800

    mt-wifi:remove disable too many logs.

--- package/mtk/applications/mtwifi-cfg/files/netifd/mtwifi.sh
+++ package/mtk/applications/mtwifi-cfg/files/netifd/mtwifi.sh
@@ -47,7 +47,7 @@ mtwifi_vif_ap_config() {
 	if [ -n "$ifname" ]; then
 		logger -t "netifd-mtwifi" "add $ifname to vifidx $name"
 		wireless_add_vif "$name" "$ifname"
-		iwpriv "$ifname" set Debug=0
+		# iwpriv "$ifname" set Debug=0
 	fi
 }
 
@@ -67,7 +67,7 @@ mtwifi_vif_sta_config() {
 	if [ -n "$ifname" ]; then
 		logger -t "netifd-mtwifi" "add $ifname to vifidx $name"
 		wireless_add_vif "$name" "$ifname"
-		iwpriv "$ifname" set Debug=0
+		# iwpriv "$ifname" set Debug=0
 	fi
 }
 
